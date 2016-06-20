01b4fb0ccd29a56888c6182b22dae8b9c9ee07e8
sangram1985 <deepak_curly@yahoo.com>
2016-06-20 00:12:27 -0400

    images linked to git

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('So you are looking for Game of Thrones facts? Just say HELLO to get started.')
                 .then(() => 'speak');
         }
     },
